f3c2cdffd7567203c7552e1d269dce743a1d5e05
Joseph Lumbley <joelumbley16@gmail.com>
2023-06-01 13:50:58 -0500

    Documentation

--- Tic Tac Toe/Form1.vb
+++ Tic Tac Toe/Form1.vb
@@ -158,50 +158,50 @@ Public Class Form1
 
     End Sub
 
-    Dim gameLoopTask As Task =
-        Task.Factory.StartNew(Sub()
-                                  Try
-                                      Do While Running
+    'Dim gameLoopTask As Task =
+    '    Task.Factory.StartNew(Sub()
+    '                              Try
+    '                                  Do While Running
 
-                                          UpdateGame()
+    '                                      UpdateGame()
 
-                                          ' Invalidate the form to trigger a redraw
-                                          'If Not Me.IsDisposed AndAlso Me.IsHandleCreated Then
-                                          If Me.IsHandleCreated Then
+    '                                      ' Invalidate the form to trigger a redraw
+    '                                      'If Not Me.IsDisposed AndAlso Me.IsHandleCreated Then
+    '                                      If Me.IsHandleCreated Then
 
-                                              Me.Invoke(Sub() Me.Refresh())
+    '                                          Me.Invoke(Sub() Me.Refresh())
 
-                                          End If
-
-
-                                          ' Wait for next frame
-                                          Thread.Sleep(TimeSpan.Zero)
-
-                                          'Thread.Sleep(TimeSpan.Zero), the thread relinquishes the
-                                          'remainder of its time slice to any thread of equal priority
-                                          'that is ready to run. If there are no other threads of equal
-                                          'priority that are ready to run, execution of the current thread is not suspended.
+    '                                      End If
 
 
-                                      Loop
+    '                                      ' Wait for next frame
+    '                                      Thread.Sleep(TimeSpan.Zero)
+
+    '                                      'Thread.Sleep(TimeSpan.Zero), the thread relinquishes the
+    '                                      'remainder of its time slice to any thread of equal priority
+    '                                      'that is ready to run. If there are no other threads of equal
+    '                                      'priority that are ready to run, execution of the current thread is not suspended.
+
+
+    '                                  Loop
 
 
 
-                                  Catch ex As Exception
+    '                              Catch ex As Exception
 
-                                      End
+    '                                  End
 
-                                  End Try
+    '                              End Try
 
-                              End Sub)
+    '                          End Sub)
 
-    'Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
 
-    '    UpdateGame()
+        UpdateGame()
 
-    '    Refresh() 'Calls OnPaint Event
+        Refresh() 'Calls OnPaint Event
 
-    'End Sub
+    End Sub
 
     Private Sub UpdateGame()
 
